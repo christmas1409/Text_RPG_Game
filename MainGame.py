1f2a7d6a946be7af5aa791fefb9a0875f02d76e2
Глеб <christmas1409@yandex.ru>
2025-08-27 12:24:15 +0300

    Задача 1: Создание героя История:
    Герой, заточенный в темнице, слышит грохот и чувствует, как стены тюрьмы начинают рушиться. Это дракон! Герой понимает, что это его шанс на свободу. Но чтобы выжить, ему нужно вспомнить, кто он есть, и подготовиться к предстоящим испытаниям.
    
    Напишите программу, которая запрашивает у пользователя:
    
    Имя героя (строка).
    
    Класс героя (строка).
    
    Здоровье (целое число).
    
    Броню (целое число).
    
    Базовый урон героя (дробное число).

---
+++ MainGame.py
@@ -0,0 +1,7 @@
+name = input()
+hero_class = input()
+health = int(input())
+armor = int(input())
+damage = float(input())
+
+print(f"=== Создание героя ===\n\nИмя: {name}\nКласс: {hero_class}\nЗдоровье: {health}, Броня: {armor}, Урон: {damage}")
