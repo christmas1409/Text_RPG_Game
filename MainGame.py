1b0753dd9e3062f3534c2d25aa793817660974e7
Глеб <christmas1409@yandex.ru>
2025-08-27 13:09:27 +0300

    Revert "Задача 1: Создание героя"
    This reverts commit 1f2a7d6a946be7af5aa791fefb9a0875f02d76e2.

--- MainGame.py
+++
@@ -1,7 +0,0 @@
-name = input()
-hero_class = input()
-health = int(input())
-armor = int(input())
-damage = float(input())
-
-print(f"=== Создание героя ===\n\nИмя: {name}\nКласс: {hero_class}\nЗдоровье: {health}, Броня: {armor}, Урон: {damage}")
