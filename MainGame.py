66f90565ba56cee95fc46a8ea93886186538eef1
Глеб <christmas1409@yandex.ru>
2025-08-27 12:26:29 +0300

    Задача 2: Магическое противостояние История:
    Герой выбирается из руин темницы, но на его пути встаёт стражник. Стражник, вооружённый мечом и щитом, готов сразиться с беглецом. Герой решает использовать магическую атаку, чтобы одолеть врага.
    
    Напишите программу, которая:
    
    Запрашивает у пользователя характеристики стражника:
    Здоровье стражника (целое число)
    Броня стражника (целое число)
    Базовый урон стражника (дробное число)
    Вычисляет урон, который герой наносит стражнику магической атакой:
    Урон героя = (Базовый урон героя × 1.5) - Броня стражника
    
    Уменьшает здоровье стражника на нанесённый урон и выводит результат.
    Добавьте ответный удар:
    
    Рассчитайте урон, который стражник наносит герою:
    Урон стражника = (Базовый урон стражника × 1.2) - Броня героя
    
    Уменьшите здоровье героя на нанесённый урон.

--- MainGame.py
+++ MainGame.py
@@ -4,4 +4,16 @@ health = int(input())
 armor = int(input())
 damage = float(input())
 
-print(f"=== Создание героя ===\n\nИмя: {name}\nКласс: {hero_class}\nЗдоровье: {health}, Броня: {armor}, Урон: {damage}")
+print(f"=== Создание героя ===\n\nИмя: {name}\nКласс: {hero_class}\nЗдоровье: {health}, "
+      f"Броня: {armor}, Урон: {damage}\n")
+
+guard_health = int(input())  # Здоровье стражника
+guard_armor = int(input())  # Броня стражника
+guard_damage = float(input())  # Базовый урон стражника
+
+magic_damage = (damage * 1.5) - guard_armor
+guard_attack = (guard_damage * 1.2) - armor
+
+print(f"=== Магическое противостояние ===\n\n{name} использовал магическую атаку и нанёс стражнику "
+      f"{round(magic_damage, 1)} урона!\nЗдоровье стражника теперь: {round(guard_health - magic_damage, 1)}\n"
+      f"Стражник атаковал героя и нанёс {round(guard_attack, 1)} урона!\nЗдоровье героя теперь: {health - guard_attack}")
