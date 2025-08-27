db741281a066d78955315b6226d49302b3c131c9
Глеб <christmas1409@yandex.ru>
2025-08-27 13:09:09 +0300

    Revert "Задача 4: Я свободен!"
    This reverts commit 6c0d87ba6f36a9cd79795eb4bd77d84c1055bd80.

--- MainGame.py
+++ MainGame.py
@@ -29,11 +29,4 @@ total_weight_after = gold_coins * 31.103 + silver_coins * 16.4 + copper_coins * 
 print(f"=== Лут ===\n\nВес до обмена: {round(total_weight_before, 2)}\nЗолото: {gold_coins}, Серебро: {silver_coins}, "
       f"Медь: {copper_coins}\nВес после обмена: {round(total_weight_after, 2)}")
 
-import math
 
-x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
-
-distance = math.sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2))
-
-print(f"=== Я свободен! ===\n\nРасстояние между мной ({x1}; {y1}) и ближайшим поселением ({x2}; {y2}) составляет "
-      f"{round(distance, 2)} точек")
