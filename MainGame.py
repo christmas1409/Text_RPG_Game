a6d7d0b90be4b6a973cee209e82059253cb5dafc
Глеб <christmas1409@yandex.ru>
2025-08-27 12:27:04 +0300

    Задача 3: Лут История:
    После победы над стражником герой находит его кошель, полный медных монет. Однако герой понимает, что нести такой груз слишком тяжело. Он решает обменять монеты на более ценные золотые и серебряные, чтобы уменьшить вес.
    
    Условия обмена:
    
    1 золотая монета = 12 серебряных монет
    1 серебряная монета = 28 медных монет
    Таким образом, 1 золотая монета = 336 медных монет
    Вес монет:
    
    1 медная монета = 25,6 единиц веса
    1 серебряная монета = 16,40 единиц веса
    1 золотая монета = 31,103 единиц веса
    Напишите программу, которая:
    
    Считает количество найденных медных монет (натуральное число).
    Рассчитает:
    Общий вес монет до обмена (с округлением до 2 знаков после запятой).
    Сколько золотых монет можно получить.
    Сколько оставшихся медяков можно обменять на серебряные монеты.
    Сколько медяков останется после этого.
    Общий вес монет после обмена (с округлением до 2 знаков после запятой).

--- MainGame.py
+++ MainGame.py
@@ -17,3 +17,16 @@ guard_attack = (guard_damage * 1.2) - armor
 print(f"=== Магическое противостояние ===\n\n{name} использовал магическую атаку и нанёс стражнику "
       f"{round(magic_damage, 1)} урона!\nЗдоровье стражника теперь: {round(guard_health - magic_damage, 1)}\n"
       f"Стражник атаковал героя и нанёс {round(guard_attack, 1)} урона!\nЗдоровье героя теперь: {health - guard_attack}")
+
+copper_coins = int(input())
+
+total_weight_before = copper_coins * 25.6
+gold_coins = round(copper_coins / 336)
+silver_coins = round((copper_coins - gold_coins * 336) / 28)
+copper_coins -= (gold_coins * 336 + silver_coins * 28)
+total_weight_after = gold_coins * 31.103 + silver_coins * 16.4 + copper_coins * 25.6
+
+print(f"=== Лут ===\n\nВес до обмена: {round(total_weight_before, 2)}\nЗолото: {gold_coins}, Серебро: {silver_coins}, "
+      f"Медь: {copper_coins}\nВес после обмена: {round(total_weight_after, 2)}")
+
+
